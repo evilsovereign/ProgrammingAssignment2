53f1d53f44f48c0987ac0ef32e904ecb2c7f6aa7
evilsovereign <emiel.hall@colorado.edu>
2014-12-20 14:13:11 -0700

    fixed typo in makeCacheMatrix comments

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,7 @@
 ## The function makeCacheMatrix creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {             ## input x will be a matrix
-        m <- NULL                                       ## m will be solved matrix, reset to NULL every time makeVector is called
+        m <- NULL                                       ## m will be solved matrix, reset to NULL every time makeCacheMatrix is called
         set <- function(y) {                            ## takes input matrix y
                 x <<- y                                 ## saves input matrix through superassignment
                 m <<- NULL                              ## resets solved matrix to NULL
